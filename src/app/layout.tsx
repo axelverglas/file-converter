a802bc191002a6ea4664b9251b93385eba68e240
Axel verglas <axelverglas@outlook.com>
2024-04-25 14:44:39 +0200

    Mise à jour de la page d'accueil avec les métadonnées du convertisseur de fichiers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/app/globals.css";
 import { Inter as FontSans } from "next/font/google";
+import type { Metadata } from "next";
 
 import { cn } from "@/lib/utils";
 import { SiteHeader } from "@/components/layout/site-header";
@@ -13,6 +14,11 @@ const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "File Converter",
+  description: `File Converter is a free online tool that allows you to convert any file to the format you need. Convert video, audio, image, and document files to any format you need.`,
+};
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
